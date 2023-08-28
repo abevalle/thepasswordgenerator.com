d91da44ab8d823eb8ee0b93855b3749b017b8994
abe valle <abe@valle.us>
2023-08-28 16:04:50 -0500

    updated looks not ready yet but it is funcitonal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import CreateForm from "./CreateForm"
 
 export default function Home() {
-  
+
   return (
-    <main className="">
-      <h1>The Password Generator</h1>
+    <main className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
+      <div className="">
+        <h1>thepasswordgenerator.com</h1>
+      </div>      
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-4HNF0DRJMG"></script>
+
       <CreateForm/>
     </main>
   )
