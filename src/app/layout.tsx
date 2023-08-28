2d4d980f866179a52b01616e81614b961dfb9bd9
abe valle <abe@valle.us>
2023-08-28 17:50:37 -0500

    added google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-4HNF0DRJMG"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-4HNF0DRJMG');
+      </script>
     </html>
   )
 }
