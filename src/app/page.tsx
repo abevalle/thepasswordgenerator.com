379494dd0ca52de3adcd98a08948a717648c6d7a
abe valle <abe@valle.us>
2023-08-28 20:02:09 -0500

    made header text smaller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
       <section className="content">
         <div className="flex flex-row items-center justify-center">
-          <h1 className="text-3xl mb-12">thepasswordgenerator.com</h1>
+          <h1 className="text-2xl mb-12">thepasswordgenerator.com</h1>
         </div>      
         <CreateForm/>
       </section>
