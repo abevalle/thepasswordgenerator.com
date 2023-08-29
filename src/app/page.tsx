60c0c4aa1db574c5c2467b7805eb3d4e64a091c8
abe valle <abe@valle.us>
2023-08-28 20:00:02 -0500

    added title and meta tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import CreateForm from "./CreateForm"
 import About from "./about"
 import Footer from "./footer"
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <main className="min-w-screen min-h-screen bg-gray-900">
+      <Head>
+        <title>The Password Generator</title>  
+        <meta name="description" content="Free password generator online. Secure password generation all within your browser! This website runs locally and will only reach out to the internet for analytics purposes"/>
+      </Head>
       <section className="content">
         <div className="flex flex-row items-center justify-center">
           <h1 className="text-3xl mb-12">thepasswordgenerator.com</h1>
